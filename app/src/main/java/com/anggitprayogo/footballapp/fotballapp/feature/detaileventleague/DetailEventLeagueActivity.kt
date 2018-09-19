50a89ebe4c4b4e136eb0675241b451b4234e4aaa
Anggit Prayogo <anggitprayogo@gmail.com>
2018-09-19 10:57:49 +0700

    buat crud anko sqlite pada detail event

--- app/src/main/java/com/anggitprayogo/footballapp/fotballapp/feature/detaileventleague/DetailEventLeagueActivity.kt
+++ app/src/main/java/com/anggitprayogo/footballapp/fotballapp/feature/detaileventleague/DetailEventLeagueActivity.kt
@@ -1,7 +1,9 @@
 package com.anggitprayogo.footballapp.fotballapp.feature.detaileventleague
 
+import android.database.sqlite.SQLiteConstraintException
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.util.Log
 import android.view.Menu
@@ -9,12 +11,19 @@ import android.view.MenuItem
 import android.widget.Toast
 import com.anggitprayogo.footballapp.fotballapp.R
 import com.anggitprayogo.footballapp.fotballapp.config.Config
+import com.anggitprayogo.footballapp.fotballapp.database.database
 import com.anggitprayogo.footballapp.fotballapp.model.detailevents.DetailEvent
 import com.anggitprayogo.footballapp.fotballapp.model.detailevents.DetailEventLeagueResponse
 import com.anggitprayogo.footballapp.fotballapp.model.detailteams.DetailTeam
+import com.anggitprayogo.footballapp.fotballapp.model.favourite.FavouriteEvent
 import com.anggitprayogo.footballapp.fotballapp.network.repository.MatchRepository
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_detail_event_league.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.delete
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
+import org.jetbrains.anko.toast
 
 class DetailEventLeagueActivity : AppCompatActivity(), DetailEventLeagueView{
 
@@ -22,7 +31,15 @@ class DetailEventLeagueActivity : AppCompatActivity(), DetailEventLeagueView{
 
     var detailevent: MutableList<DetailEvent> = mutableListOf()
     var idEvent: String = ""
+    var nameEvent: String? = null
+    var homeScoreEvent: String? = null
+    var homeNameEvent: String? = null
+    var awayScoreEvent: String? = null
+    var awayNameEvent: String? = null
+    var dateEvent: String? = null
+    var timeEvent: String? = null
     var menuItem: Menu? = null
+    var favourite: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +57,8 @@ class DetailEventLeagueActivity : AppCompatActivity(), DetailEventLeagueView{
 
         presenter.getDetailMatch(idEvent)
 
+        checkFavourite();
+
         swipe_refresh_layout.setOnRefreshListener {
             swipe_refresh_layout.isRefreshing = false
             presenter.getDetailMatch(idEvent)
@@ -56,8 +75,20 @@ class DetailEventLeagueActivity : AppCompatActivity(), DetailEventLeagueView{
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when(item?.itemId){
             R.id.nav_favourite -> {
-                Toast.makeText(this, "Favourite", Toast.LENGTH_SHORT).show()
-               return true
+
+                if (favourite!!) {
+                    removeFromFavourite()
+                    favourite = false
+                } else {
+                    // add to favorite
+                    insertToFavourite()
+                    favourite = true
+                }
+
+                //set favorite
+                setFavourite(favourite!!)
+
+                return true
             }
             R.id.home ->{
                 finish()
@@ -83,6 +114,14 @@ class DetailEventLeagueActivity : AppCompatActivity(), DetailEventLeagueView{
         Log.d("DATAS ", ""+datas.size)
 
         var model = datas.get(0)
+
+        nameEvent = model.strEvent
+        homeScoreEvent = model.intHomeScore
+        homeNameEvent = model.strHomeTeam
+        awayNameEvent = model.strAwayTeam
+        awayScoreEvent = model.intAwayScore
+        dateEvent = model.strEvent
+        timeEvent = model.strTime
 
         tv_date.text = model.strDate
         tv_hours.text = model.strTime
@@ -137,5 +176,90 @@ class DetailEventLeagueActivity : AppCompatActivity(), DetailEventLeagueView{
     }
 
     override fun showDetailAwayTeamError() {
+
+    }
+
+
+
+    fun setFavourite(favourite: Boolean){
+
+        Log.d("FAVOURITE ", ""+favourite)
+
+        if (favourite) {
+            Log.d("FAVOURITE ", "Ada")
+
+            menuItem?.getItem(0)?.icon =
+                    ContextCompat.getDrawable(this, R.drawable.ic_favorite_black_24dp)
+        } else {
+            Log.d("FAVOURITE ", "Gak ada")
+
+            menuItem?.getItem(0)?.icon =
+                    ContextCompat.getDrawable(this, R.drawable.ic_favorite_border_black_24dp)
+        }
+    }
+
+    fun checkFavourite() {
+        try {
+
+            database.use {
+                val queryBuilder = select(FavouriteEvent.TABLE_FAVOURITE_EVENT)
+                        .whereArgs("(${FavouriteEvent.EVENT_ID} = {id})",
+                                "id" to idEvent)
+                val result = queryBuilder.parseList(classParser<FavouriteEvent>())
+
+                favourite = !result.isEmpty()
+                setFavourite(favourite!!)
+            }
+
+        }catch (e: SQLiteConstraintException){
+            toast(e.message!!)
+        }
+    }
+
+    fun insertToFavourite(){
+
+        try {
+
+            database.use {
+                insert(FavouriteEvent.TABLE_FAVOURITE_EVENT,
+                        FavouriteEvent.EVENT_ID to idEvent,
+                        FavouriteEvent.EVENT_NAME to nameEvent,
+                        FavouriteEvent.HOME_NAME to homeNameEvent,
+                        FavouriteEvent.HOME_SCORE to homeScoreEvent,
+                        FavouriteEvent.AWAY_NAME to awayNameEvent,
+                        FavouriteEvent.AWAY_SCORE to awayScoreEvent,
+                        FavouriteEvent.DATE to dateEvent,
+                        FavouriteEvent.TIME to timeEvent)
+            }
+
+            Toast.makeText(this, getString(R.string.message_favourite_add_success), Toast.LENGTH_LONG).show()
+
+        }catch (e: SQLiteConstraintException){
+
+            Toast.makeText(this, e?.message, Toast.LENGTH_LONG).show()
+
+        }
+
+    }
+
+    fun removeFromFavourite(){
+
+        try {
+
+            database.use {
+                delete(FavouriteEvent.TABLE_FAVOURITE_EVENT,
+                        "( ${FavouriteEvent.EVENT_ID} = {event_id})",
+                        "event_id" to idEvent)
+            }
+
+            Toast.makeText(this, getString(R.string.message_favourite_remove_success), Toast.LENGTH_LONG).show()
+
+
+        }catch (e: SQLiteConstraintException){
+
+            Toast.makeText(this, e?.message, Toast.LENGTH_LONG).show()
+
+        }
+
     }
 }
